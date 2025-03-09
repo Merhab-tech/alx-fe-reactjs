c3ed2c300d06864fe8854860af9039316ed681d0
Rohobot <rohobotgirma@gmail.com>
2025-03-09 21:11:15 +0300

    initial commit

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import FormikForm from './components/FormikForm';
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/formikForm';
 
 function App() {
   return (
-    <div className="App">
-      <h1>User Registration with Formik</h1>
+    <div>
+      <h1>User Registration</h1>
+      <h2>Controlled Components Form</h2>
+      <RegistrationForm />
+      <h2>Formik Form</h2>
       <FormikForm />
     </div>
   );
